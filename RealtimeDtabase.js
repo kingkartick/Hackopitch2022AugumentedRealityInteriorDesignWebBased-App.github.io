65fe32ed59ff4b1bfbdcb8bc893bca43080340dc
kingkartick <79476545+kingkartick@users.noreply.github.com>
2022-05-07 17:43:04 +0530

    Update RealtimeDtabase.js

--- RealtimeDtabase.js
+++ RealtimeDtabase.js
@@ -1,5 +1,5 @@
  
-                  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+                  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js"
                   import { getDatabase ,ref,set } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
               
                   const firebaseConfig = {
