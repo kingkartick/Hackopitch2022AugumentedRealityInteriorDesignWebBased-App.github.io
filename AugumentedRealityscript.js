498edff0631eb416efbdfe31ef0f0831c1fc0ce1
kingkartick <79476545+kingkartick@users.noreply.github.com>
2022-05-07 17:45:55 +0530

    Update AugumentedRealityscript.js

--- AugumentedRealityscript.js
+++ AugumentedRealityscript.js
@@ -1,6 +1,6 @@
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
 import { getAuth, signInWithPopup, GoogleAuthProvider} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
